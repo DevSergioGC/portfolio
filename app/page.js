7d03471d0a614839fb4524ae68b7814ff54c3e1a
Sergio Guzman <sergiocamachog12@gmail.com>
2023-12-19 14:35:10 -0400

    feature: add dark mode
    Implement theme button to change between light & dark mode

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import { useDarkMode } from "../app/components/DarkModeContext.jsx";
 
 export default function Home() {
+  const { darkMode } = useDarkMode();
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className={`flex min-h-screen flex-col ${darkMode ? `dark-mode` : `light-mode`}`}>
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
