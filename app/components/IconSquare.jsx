406c02a297663d2c4976de493c49a05af6461172
DevSergioGC <sergiocamachog12@gmail.com>
2024-01-01 22:04:54 -0400

    refactor: simplify iconSquare logic

--- app/components/IconSquare.jsx
+++ app/components/IconSquare.jsx
@@ -1,56 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
-import ModalComponent from "./ModalComponent";
 
-const IconSquare = ({ name, tab, key }) => {
-  const boxStyle = [
-    "dark:bg-[#181818] bg-light-shadow-box rounded-md p-4 dark:text-white text-black flex flex-col items-center",
-    "dark:bg-[#181818] bg-light-shadow-box rounded-md p-4 dark:text-white text-black flex flex-col items-center dark:hover:bg-gray-500 hover:bg-[#C0C0C0] hover:cursor-pointer",
-  ];
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
+const IconSquare = ({ data }) => {
   return (
-    <div>
-      <div index={key} className={tab !== "skills" ? boxStyle[1] : boxStyle[0]}>
-        {tab === "skills" && (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      {data.map((item, index) => (
+        <div
+          index={index}
+          className="dark:bg-[#181818] bg-light-shadow-box rounded-md p-4 dark:text-white text-black flex flex-col items-center"
+          key={index}
+        >
           <div className="icon w-12 h-12 mb-2">
             <Image
-              src={`/images/icons/${name.toLowerCase()}-logo.png`}
-              alt={`${tab} - ${name}`}
-              width={45}
-              height={25}
+              src={`/images/icons/${item.name.toLowerCase()}-logo.png`}
+              alt={`${item.name}`}
+              width={50}
+              height={50}
             />
           </div>
-          
-        )}
-        {/* {isModalOpen && tab != "skills" && (
-          <ModalComponent onClose={closeModal}>
-            <Image
-              src={`/images/certificates/${name
-                .trim()
-                .replace(/\s/g, "")
-                .toLowerCase()}.png`}
-              alt={`${tab} - ${name}`}
-              className="w-full h-auto"
-              width={500}
-              height={500}
-            />
-          </ModalComponent>
-        )} */}
-        <div
-          className="text-center"
-          onClick={tab != "skill" ? openModal : null}
-        >
-          {name}
+          <div className="text-center">{item.name}</div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
